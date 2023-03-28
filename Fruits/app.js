93020fef78e74edac42f6b8da837e3eceae64191
DevMansuri <nadimmansu@gmail.com>
2023-03-28 21:25:42 +0530

    Update file :add findDocument funtion for find the data

--- Fruits/app.js
+++ Fruits/app.js
@@ -9,7 +9,7 @@ client.connect(function(err){
     assert.equal(null ,err);
     console.log("Connected succesfully");
     const db =  client.db(dbName);
-    insertDocuments(db ,function() {
+    findDocuments(db ,function() {
         client.close();
     });
     
@@ -41,5 +41,15 @@ const insertDocuments = function(db, callback) {
         assert.equal(3,result.ops.length);
         console.log("Inserted 3 data");
         callback(result);
+    });
+};
+
+const findDocuments = function(db , callback) {
+    const collection = db.collection('fruits');
+    collection.find({}).toArray(function(err,fruits) {
+        assert.equal(err,null);
+        console.log("find following");
+        console.log(fruits);
+        callback(fruits);
     });
 }
