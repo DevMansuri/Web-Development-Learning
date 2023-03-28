22e83499c9058f407b93b076d8f8e78e814f2518
DevMansuri <nadimmansu@gmail.com>
2023-03-28 21:15:26 +0530

    Add new project : Native mongodb with Nodejs

---
+++ Fruits/app.js
@@ -0,0 +1,45 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = 'mongodb://localhost:27017';
+const dbName = 'fruitsDB';
+
+const client = new MongoClient(url , {useNewUrlParser: true });
+
+client.connect(function(err){
+    assert.equal(null ,err);
+    console.log("Connected succesfully");
+    const db =  client.db(dbName);
+    insertDocuments(db ,function() {
+        client.close();
+    });
+    
+});
+
+const insertDocuments = function(db, callback) {
+    const collection = db.collection('fruits');
+    collection.insertMany([
+        {
+            name: 'Apple',
+            score: 5,
+            review: 'osm'
+        },
+        {
+            name: 'Bee',
+            score: 5,
+            review: 'osm'
+
+        },
+        {
+            name: 'mpple',
+            score: 5,
+            review: 'osm'
+
+        }
+    ],function(err ,result) {
+        assert.equal(err,null);
+        assert.equal(3,result.result.n);
+        assert.equal(3,result.ops.length);
+        console.log("Inserted 3 data");
+        callback(result);
+    });
+}
